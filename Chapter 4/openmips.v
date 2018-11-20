a894efb26a3b0896222dc73eb7cfe12220993309
WYMT <lepetitprince517@sjtu.edu.cn>
2018-11-20 23:41:44 +0800

    sync

--- Chapter 4/openmips.v
+++ Chapter 4/openmips.v
@@ -1,1 +1,12 @@
+`include"defines.v"
+module openmips(
 
+    input wire                  clk,
+    input wire                  rst,
+
+    input wire[`RegBus]         rom_data_i,
+    output wire[`RegBus]        rom_data_o,
+    output wire                 rom_ce_o    
+);
+
+endmodule
